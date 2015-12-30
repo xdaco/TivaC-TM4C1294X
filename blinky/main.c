abd7b7417405a7ececbb2f6f279ff48d57644ef2
xdaco <xdaco.xinu@gmail.com>
2015-12-30 16:34:08 +0530

    First Working code !!

--- blinky/main.c
+++ blinky/main.c
@@ -24,17 +24,13 @@
 #include "driverlib/udma.h"
 #include "driverlib/gpio.h"
 #include "driverlib/timer.h"
+#include "utils/uartstdio.h"
 #include <string.h>
 
 //************** Include Section Ends here************************************//
 
-#define LEDbase GPIO_PORTF_BASE
-#define LEDred GPIO_PIN_1
-#define LEDblue GPIO_PIN_2
-#define LEDgreen GPIO_PIN_3
-
-void InitConsole(void)
-{
+//*************** Setting UART7 as debug Port*********************************//
+void InitConsole(void){
 
 		SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
 	    GPIOPinConfigure(GPIO_PC4_U7RX);
@@ -45,16 +41,51 @@ void InitConsole(void)
 	    UARTStdioConfig(7, 115200, 16000000);
 }
 
+//*************** Setting UART7 as debug Port Ends here*********************************//
 
 int main(){
 
-  SysCtlClockSet(SYSCTL_SYSDIV_2_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-  SysCtlDelay(16000000 / 3); /* 1 Second Delay */
-  GPIOPinTypeGPIOOutput(LEDbase, LEDred|LEDblue|LEDgreen);
-  GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4);
+		SysCtlClockSet(SYSCTL_SYSDIV_2_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
+		SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // Enables PORT F
+		SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOH); // Enables PORT H
+		SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOM); // Enables PORT M
+		SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOL); // Enables PORT L
+		GPIOPinTypeGPIOOutput(GPIO_PORTF_AHB_BASE, 0x0E); //PF1 , PF2, PF3 are set as Output
+		GPIOPinTypeGPIOOutput(GPIO_PORTH_AHB_BASE, 0x06); //PH1, PH2 are set as output
+		GPIOPinTypeGPIOOutput(GPIO_PORTM_BASE, 0x02); // PM1 is set as output
+		GPIOPinTypeGPIOOutput(GPIO_PORTL_BASE, 0x30); // PL4,PL5 are set as output
+		GPIOPadConfigSet(GPIO_PORTF_AHB_BASE, 0x0E, GPIO_STRENGTH_8MA, GPIO_PIN_TYPE_STD); // Requires only for PORT H & F
+		GPIOPadConfigSet(GPIO_PORTH_AHB_BASE, 0x06, GPIO_STRENGTH_8MA, GPIO_PIN_TYPE_STD); // Requires only for PORT H & F
+
+		InitConsole(); // Calls the UART debug config funtion
+		UARTprintf("Blinky test program for TM4C1294X\n"); // General print for the code information.
+
 
     while(1){
+
+    	UARTprintf("Making PF1,PF2,PF3,PH1,PH2,PM1,PL4 and PL5 high\n");
+    	GPIOPinWrite(GPIO_PORTF_AHB_BASE,GPIO_PIN_1, GPIO_PIN_1);
+    	GPIOPinWrite(GPIO_PORTF_AHB_BASE,GPIO_PIN_2, GPIO_PIN_2);
+    	GPIOPinWrite(GPIO_PORTF_AHB_BASE,GPIO_PIN_3, GPIO_PIN_3);
+    	GPIOPinWrite(GPIO_PORTH_AHB_BASE,GPIO_PIN_1, GPIO_PIN_1);
+    	GPIOPinWrite(GPIO_PORTH_AHB_BASE,GPIO_PIN_2, GPIO_PIN_2);
+    	GPIOPinWrite(GPIO_PORTM_BASE, GPIO_PIN_1, GPIO_PIN_1);
+    	GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_4, GPIO_PIN_4);
+    	GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_5, GPIO_PIN_5);
+
+    	SysCtlDelay((16000000*3) / 3); // 3 second delay
+
+    	UARTprintf("Making PF1,PF2,PF3,PH1,PH2,PM1,PL4 and PL5 Low\n");
+    	GPIOPinWrite(GPIO_PORTF_AHB_BASE,GPIO_PIN_1, ~GPIO_PIN_1);
+    	GPIOPinWrite(GPIO_PORTF_AHB_BASE,GPIO_PIN_2, ~GPIO_PIN_2);
+    	GPIOPinWrite(GPIO_PORTF_AHB_BASE,GPIO_PIN_3, ~GPIO_PIN_3);
+    	GPIOPinWrite(GPIO_PORTH_AHB_BASE,GPIO_PIN_1, ~GPIO_PIN_1);
+    	GPIOPinWrite(GPIO_PORTH_AHB_BASE,GPIO_PIN_2, ~GPIO_PIN_2);
+    	GPIOPinWrite(GPIO_PORTM_BASE, GPIO_PIN_1, ~GPIO_PIN_1);
+    	GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_4, ~GPIO_PIN_4);
+    	GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_5, ~GPIO_PIN_5);
+
+    	SysCtlDelay((16000000*3) / 3); // 3 second delay
 
 
       }
